0f4282301cdf385eaf509d0849876c127807be7c
Andreas Haas <dra.haas@gmail.com>
2013-09-26 14:33:07 +0200

    benchmark script

--- tools/script.sh
+++ tools/script.sh
@@ -1,12 +1,18 @@
+benchmarks='prodcon'
+
 for i in 1 2 3 4 5 
+#for i in 1 
 do 
-  for b in prodcon seqalt enq deq
+#  for b in prodcon # seqalt # enq deq # infprod
+  for b in $benchmarks
   do 
-    python b8_benchmark.py ${b} /home/ahaas/ppopp14/b8/results/${b}${i}/  
+    python b8_benchmark.py ${b} /home/ahaas/ppopp14/b8/results1m/${b}${i}/  
   done 
 done
-for b in prodcon seqalt enq deq
+for b in $benchmarks
 do 
-  python calc_average.py /home/ahaas/ppopp14/b8/results/${b}/ /home/ahaas/ppopp14/b8/results/${b}1/ /home/ahaas/ppopp14/b8/results/${b}2/ /home/ahaas/ppopp14/b8/results/${b}3/ /home/ahaas/ppopp14/b8/results/${b}4/ /home/ahaas/ppopp14/b8/results/${b}5/  
-  python make_data_files_threads.py /home/ahaas/ppopp14/b8/results/${b}/  
+#  python calc_average.py /home/ahaas/ppopp14/b8/results1m/${b}/ /home/ahaas/ppopp14/b8/results1m/${b}1/
+  python calc_average.py /home/ahaas/ppopp14/b8/results1m/${b}/ /home/ahaas/ppopp14/b8/results1m/${b}1/ /home/ahaas/ppopp14/b8/results1m/${b}2/ /home/ahaas/ppopp14/b8/results1m/${b}3/ /home/ahaas/ppopp14/b8/results1m/${b}4/ /home/ahaas/ppopp14/b8/results1m/${b}5/  
+  python make_data_files_threads.py /home/ahaas/ppopp14/b8/results1m/${b}/  
+#  python make_data_files_load.py /home/ahaas/ppopp14/b8/results1m/${b}/  
 done 
