cd0c3d4d2d4ae9afd6c9359c6d4b9753d6da0699
Andreas Haas <dra.haas@gmail.com>
2014-01-17 12:11:45 +0100

    typo fixed

--- tools/script.sh
+++ tools/script.sh
@@ -15,5 +15,5 @@ do
 #  python calc_average.py /home/ahaas/sbg14/b8/results1m/${b}/ /home/ahaas/sbg14/b8/results1m/${b}1/
   python calc_average.py /home/ahaas/sbg14/b8/results1m/${b}/ /home/ahaas/sbg14/b8/results1m/${b}1/ /home/ahaas/sbg14/b8/results1m/${b}2/ /home/ahaas/sbg14/b8/results1m/${b}3/ /home/ahaas/sbg14/b8/results1m/${b}4/ /home/ahaas/sbg14/b8/results1m/${b}5/  
 #  python make_data_files_threads.py /home/ahaas/sbg14/b8/results1m/${b}/  
-  python make_data_files_load.py /home/ahaas/sbg4/b8/results1m/${b}/  
+  python make_data_files_load.py /home/ahaas/sbg14/b8/results1m/${b}/  
 done 
